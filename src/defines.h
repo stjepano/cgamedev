b05af8c7da704ac6a72f0ad14b9bac6d4cca6900
Stjepan Obrankovic <stjepano@gmail.com>
2017-04-13 00:43:36 +0100

    Define basic types

--- src/defines.h
+++ src/defines.h
@@ -8,6 +8,11 @@
 #define NULL ((void*)0)
 #endif
 
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
 typedef int bool_t;
 
 #ifndef TRUE
