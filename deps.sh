cd58c09587ec8fcd8e9f1be0a5c552235e102e9d
Stjepan Obrankovic <stjepano@gmail.com>
2017-04-14 10:19:17 +0100

    Fix dependency download script and Makefile to support new glew

--- deps.sh
+++ deps.sh
@@ -5,17 +5,25 @@ rm -rf 3rdparty/
 
 # Download SDL2
 curl -O https://www.libsdl.org/release/SDL2-devel-2.0.5-mingw.tar.gz
-curl -L -o glew-2.0.0-win32.zip https://sourceforge.net/projects/glew/files/glew/2.0.0/glew-2.0.0-win32.zip/download
+curl -L -o glew-2.0.0.tgz https://sourceforge.net/projects/glew/files/glew/2.0.0/glew-2.0.0.tgz/download
+
 mkdir temp
 mv SDL2-devel-2.0.5-mingw.tar.gz temp/
-mv glew-2.0.0-win32.zip temp/
+mv glew-2.0.0.tgz temp/
+
 cd temp/
 tar -zxvf SDL2-devel-2.0.5-mingw.tar.gz
-unzip glew-2.0.0-win32.zip
+tar -zxvf glew-2.0.0.tgz
+
 mkdir ../3rdparty
+
 cp -R SDL2-2.0.5/i686-w64-mingw32/* ../3rdparty/
-cp -R glew-2.0.0/bin/Release/Win32/* ../3rdparty/bin/
-cp -R glew-2.0.0/lib/Release/Win32/* ../3rdparty/lib/
-cp -R glew-2.0.0/include/* ../3rdparty/include/
-cd ..
+
+# install glew
+cd glew-2.0.0
+make glew.lib
+cp -R include/* ../../3rdparty/include/
+cp -R lib/* ../../3rdparty/lib/
+
+cd ../..
 rm -rf temp/
