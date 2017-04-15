414bb24d8065cf4e350542ec4296170760d9d142
Stjepan Obrankovic <stjepano@gmail.com>
2017-04-15 20:47:45 +0100

    Triangle drawing using indices

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@
 bool    g_quit      = false;
 GLuint  g_vao       = 0;
 GLuint  g_vbo       = 0;
+GLuint  g_ebo       = 0;
 GLuint  g_program   = 0;
 
 error_t InitOpenGL();
@@ -98,7 +99,7 @@ void Render()
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(g_program);
     glBindVertexArray(g_vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
 
@@ -127,6 +128,8 @@ error_t InitOpenGL()
         0.0f,  0.5f, 0.0f
     };
 
+    GLuint indices[] = {0, 1, 2};
+
     GLuint vertex_shader, fragment_shader;
     if ( FAILED(GLT_CompileShader(GL_VERTEX_SHADER, VERTEX_SHADER_SOURCE, &vertex_shader)) )
     {
@@ -149,6 +152,10 @@ error_t InitOpenGL()
     glGenBuffers(1, &g_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &g_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
