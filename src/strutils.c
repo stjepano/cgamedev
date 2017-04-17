eb269be29fdabff534ef2010e678cadf02a04be7
Stjepan Obrankovic <stjepano@gmail.com>
2017-04-17 23:14:06 +0100

    Add resolution parsing function to strutils.h

--- src/strutils.c
+++ src/strutils.c
@@ -3,10 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define STR_ERR_CODE 1177
+#define RESOLUTION_DIVIDER_CHAR 'x'
+
 char* STR_strdup(const char* str)
 {
     char* newstr = (char*) malloc(sizeof(char)*(strlen(str)+1));
     newstr[0] = 0;
     strcpy(newstr, str);
     return newstr;
+}
+
+error_t STR_ParseResolution(const char* str, int* width, int* height)
+{
+    const char* resptr = strchr(str, RESOLUTION_DIVIDER_CHAR);
+    if (resptr == NULL)
+    {
+        return Error(STR_ERR_CODE, "Invalid resolution string, expected WIDTHxHEIGHT but got '%s'", str);
+    }
+    
+    resptr++;
+    
+    int h = atoi(resptr);
+    int w = atoi(resptr);
+    
+    if (height != NULL) (*height) = h;
+    if (width != NULL) (*width) = w;
+
+    if ( h <= 0 )
+    {
+        return Error(STR_ERR_CODE, "Invalid resolution string, expected WIDTHxHEIGHT but got '%s'", str);
+    }
+    if ( w <= 0 )
+    {
+        return Error(STR_ERR_CODE, "Invalid resolution string, expected WIDTHxHEIGHT but got '%s'", str);
+    }
+
+    return NoError();
 }
--- src/strutils.h
+++ src/strutils.h
@@ -4,4 +4,11 @@
 /// Copy str into newly allocated string and return it
 char* STR_strdup(const char* str);
 
+/// Parse resolution in format WIDTHxHEIGHT, ex 1024x768
+/// If string is in invalid format or WIDTH <= 0 or HEIGHT <= 0 error will be returned
+/// If width is NULL it will not be set, string will still be parsed for height
+/// If height is NULL it will not be set, string is still parsed for width
+/// If both width and height are NULL they will not be set but string will still be parsed
+error_t STR_ParseResolution(const char* str, int* width, int* height);
+
 #endif
